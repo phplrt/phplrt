d673747bea5b09bb4940f1e68ddfcdf6f250682f
knesmeyanov <k.nesmeyanov@rambler-co.ru>
2019-09-18 16:17:16 +0300

    Optimize imports and remove subsplit binary

--- bin/build.sh
+++
@@ -1,7 +0,0 @@
-#/bin/bash
-git clone https://github.com/SerafimArts/sync-tool.git
-cd sync-tool
-composer install
-cd ../../
-php bin/sync-tool/packages sync "subsplit.json" -vvv
-rm -rf bin/sync-tool
