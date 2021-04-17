26911e534961a2376ef16b44d8b67535e6ab08eb
SerafimArts <nesk@xakep.ru>
2021-04-17 23:14:55 +0300

    Remove lexer offset

--- src/Contracts/Lexer/src/LexerInterface.php
+++ src/Contracts/Lexer/src/LexerInterface.php
@@ -23,10 +23,9 @@ interface LexerInterface
      * Returns a set of token objects from the passed source.
      *
      * @param string|resource|ReadableInterface $source
-     * @param int $offset
      * @return iterable|TokenInterface[]
      *
      * @throws RuntimeExceptionInterface
      */
-    public function lex($source, int $offset = 0): iterable;
+    public function lex($source): iterable;
 }
