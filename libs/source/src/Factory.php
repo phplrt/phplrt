9adf2c59f36974512d7292c6f8105b94638fe25c
Kostya <75450127+krvvko@users.noreply.github.com>
2022-02-01 10:30:51 -0500

    Comment line optimizations
    This update will make your project x100 more popular

--- libs/source/src/Factory.php
+++ libs/source/src/Factory.php
@@ -3,8 +3,8 @@
 /**
  * This file is part of phplrt package.
  *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code
  */
 
 declare(strict_types=1);
