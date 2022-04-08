0036ea4a4d661aa9e83859e964d919ed7620015f
knesmeyanov <knesmeyanov@fun.co>
2022-04-08 20:12:20 +0300

    Improve deprecation notice

--- libs/position/src/PositionInterface.php
+++ libs/position/src/PositionInterface.php
@@ -14,7 +14,8 @@ namespace Phplrt\Position;
 use Phplrt\Contracts\Position\PositionInterface as PositionContract;
 
 /**
- * @deprecated since 3.2 and will be removed in 4.0. Please use {@see PositionContract} instead.
+ * @deprecated since phplrt 3.2 and will be removed in 4.0.
+ *             Please use {@see PositionContract} instead.
  */
 interface PositionInterface extends PositionContract
 {
