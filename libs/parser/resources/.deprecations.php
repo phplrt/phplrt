ffeacaf88a8e1151e81de4c63cb2b1b93d2ad161
SerafimArts <nesk@xakep.ru>
2022-04-09 20:49:06 +0300

    Improve deprecation notices

--- libs/parser/resources/.deprecations.php
+++ libs/parser/resources/.deprecations.php
@@ -3,63 +3,63 @@
 namespace Phplrt\Grammar {
 
     /**
-     * @deprecated since phplrt 3.2, use {@see \Phplrt\Parser\Grammar\Terminal} instead.
+     * @deprecated since phplrt 3.2 and will be removed in 4.0, use {@see \Phplrt\Parser\Grammar\Terminal} instead.
      */
     abstract class Terminal extends \Phplrt\Parser\Grammar\Terminal
     {
     }
 
     /**
-     * @deprecated since phplrt 3.2, use {@see \Phplrt\Parser\Grammar\Repetition} instead.
+     * @deprecated since phplrt 3.2 and will be removed in 4.0, use {@see \Phplrt\Parser\Grammar\Repetition} instead.
      */
     class Repetition extends \Phplrt\Parser\Grammar\Repetition
     {
     }
 
     /**
-     * @deprecated since phplrt 3.2, use {@see \Phplrt\Parser\Grammar\Optional} instead.
+     * @deprecated since phplrt 3.2 and will be removed in 4.0, use {@see \Phplrt\Parser\Grammar\Optional} instead.
      */
     class Optional extends \Phplrt\Parser\Grammar\Optional
     {
     }
 
     /**
-     * @deprecated since phplrt 3.2, use {@see \Phplrt\Parser\Grammar\Production} instead.
+     * @deprecated since phplrt 3.2 and will be removed in 4.0, use {@see \Phplrt\Parser\Grammar\Production} instead.
      */
     abstract class Production extends \Phplrt\Parser\Grammar\Production
     {
     }
 
     /**
-     * @deprecated since phplrt 3.2, use {@see \Phplrt\Parser\Grammar\Concatenation} instead.
+     * @deprecated since phplrt 3.2 and will be removed in 4.0, use {@see \Phplrt\Parser\Grammar\Concatenation} instead.
      */
     class Concatenation extends \Phplrt\Parser\Grammar\Concatenation
     {
     }
 
     /**
-     * @deprecated since phplrt 3.2, use {@see \Phplrt\Parser\Grammar\Builder} instead.
+     * @deprecated since phplrt 3.2 and will be removed in 4.0, use {@see \Phplrt\Parser\Grammar\Builder} instead.
      */
     class Builder extends \Phplrt\Parser\Grammar\Builder
     {
     }
 
     /**
-     * @deprecated since phplrt 3.2, use {@see \Phplrt\Parser\Grammar\Rule} instead.
+     * @deprecated since phplrt 3.2 and will be removed in 4.0, use {@see \Phplrt\Parser\Grammar\Rule} instead.
      */
     class Rule extends \Phplrt\Parser\Grammar\Rule
     {
     }
 
     /**
-     * @deprecated since phplrt 3.2, use {@see \Phplrt\Parser\Grammar\Lexeme} instead.
+     * @deprecated since phplrt 3.2 and will be removed in 4.0, use {@see \Phplrt\Parser\Grammar\Lexeme} instead.
      */
     class Lexeme extends \Phplrt\Parser\Grammar\Lexeme
     {
     }
 
     /**
-     * @deprecated since phplrt 3.2, use {@see \Phplrt\Parser\Grammar\Alternation} instead.
+     * @deprecated since phplrt 3.2 and will be removed in 4.0, use {@see \Phplrt\Parser\Grammar\Alternation} instead.
      */
     class Alternation extends \Phplrt\Parser\Grammar\Alternation
     {
@@ -69,21 +69,21 @@ namespace Phplrt\Grammar {
 namespace Phplrt\Contracts\Grammar {
 
     /**
-     * @deprecated since phplrt 3.2, use {@see \Phplrt\Parser\Grammar\ProductionInterface} instead.
+     * @deprecated since phplrt 3.2 and will be removed in 4.0, use {@see \Phplrt\Parser\Grammar\ProductionInterface} instead.
      */
     interface ProductionInterface extends \Phplrt\Parser\Grammar\ProductionInterface
     {
     }
 
     /**
-     * @deprecated since phplrt 3.2, use {@see \Phplrt\Parser\Grammar\RuleInterface} instead.
+     * @deprecated since phplrt 3.2 and will be removed in 4.0, use {@see \Phplrt\Parser\Grammar\RuleInterface} instead.
      */
     interface RuleInterface extends \Phplrt\Parser\Grammar\RuleInterface
     {
     }
 
     /**
-     * @deprecated since phplrt 3.2, use {@see \Phplrt\Parser\Grammar\TerminalInterface} instead.
+     * @deprecated since phplrt 3.2 and will be removed in 4.0, use {@see \Phplrt\Parser\Grammar\TerminalInterface} instead.
      */
     interface TerminalInterface extends \Phplrt\Parser\Grammar\TerminalInterface
     {
--- libs/position/src/PositionInterface.php
+++ libs/position/src/PositionInterface.php
@@ -14,8 +14,7 @@ namespace Phplrt\Position;
 use Phplrt\Contracts\Position\PositionInterface as PositionContract;
 
 /**
- * @deprecated since phplrt 3.2 and will be removed in 4.0.
- *             Please use {@see PositionContract} instead.
+ * @deprecated since phplrt 3.2 and will be removed in 4.0, use {@see PositionContract} instead.
  */
 interface PositionInterface extends PositionContract
 {
