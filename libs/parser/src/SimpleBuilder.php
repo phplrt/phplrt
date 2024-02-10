9793f66c63f9ceadb8206f293c89976a8cb57eb9
Kirill Nesmeyanov <kirill.nesmeianov@tarkov.com>
2024-02-10 17:37:37 +0300

    Add SimpleBuilder deprecations

--- libs/parser/src/SimpleBuilder.php
+++ libs/parser/src/SimpleBuilder.php
@@ -9,4 +9,14 @@ use Phplrt\Parser\Context\TreeBuilder;
 /**
  * @deprecated since phplrt 3.4 and will be removed in 4.0
  */
-final class SimpleBuilder extends TreeBuilder {}
+final class SimpleBuilder extends TreeBuilder
+{
+    public function __construct(iterable $reducers)
+    {
+        trigger_deprecation('phplrt/parser', '3.4', <<<'MSG'
+            Using "%s::class" is deprecated, please use "%s::class" instead.
+            MSG, self::class, TreeBuilder::class);
+
+        parent::__construct($reducers);
+    }
+}
