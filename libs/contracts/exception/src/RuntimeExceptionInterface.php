6b88532fa9a9cf3bfd27461e91e5bb48dd55344b
Kirill Nesmeyanov <kirill.nesmeianov@tarkov.com>
2024-02-18 03:07:16 +0300

    Mark exception contracts as deprecated

--- libs/contracts/exception/src/RuntimeExceptionInterface.php
+++ libs/contracts/exception/src/RuntimeExceptionInterface.php
@@ -7,6 +7,10 @@ namespace Phplrt\Contracts\Exception;
 use Phplrt\Contracts\Lexer\TokenInterface;
 use Phplrt\Contracts\Source\ReadableInterface;
 
+/**
+ * @deprecated since phplrt 3.6 and will be removed in 4.0, please use specific
+ *             exception interfaces instead.
+ */
 interface RuntimeExceptionInterface extends \Throwable
 {
     public function getToken(): TokenInterface;
