49f31e246800f0909e9a30113970e90ddd0d89b4
Kirill Nesmeyanov <kirill.nesmeianov@tarkov.com>
2024-02-18 03:08:26 +0300

    Add exceptions inside parser contracts

--- libs/contracts/parser/src/ParserInterface.php
+++ libs/contracts/parser/src/ParserInterface.php
@@ -4,19 +4,26 @@ declare(strict_types=1);
 
 namespace Phplrt\Contracts\Parser;
 
-use Phplrt\Contracts\Exception\RuntimeExceptionInterface;
+use Phplrt\Contracts\Source\ReadableInterface;
 
 /**
  * An interface that implements methods for parsing source code.
+ *
+ * @template TNode of object
  */
 interface ParserInterface
 {
     /**
      * Parses sources into an abstract source tree (AST) or list of AST nodes.
      *
-     * @return iterable<array-key, object>
+     * @return iterable<array-key, TNode>
      *
-     * @throws RuntimeExceptionInterface
+     * @throws ParserExceptionInterface An error occurs before source processing
+     *         starts, when the given source cannot be recognized or if the
+     *         parser settings contain errors.
+     * @throws ParserRuntimeExceptionInterface An exception that occurs after
+     *         starting the parsing and indicates problems in the analyzed
+     *         source.
      */
-    public function parse($source): iterable;
+    public function parse(ReadableInterface $source): iterable;
 }
