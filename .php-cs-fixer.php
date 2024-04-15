38f64d8643ebafd1d9cb3305c9af822d5dbab8e4
Kirill Nesmeyanov <kirill.nesmeianov@tarkov.com>
2024-04-15 03:59:15 +0300

    Fix PER-2.0 codestyle

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -1,23 +1,18 @@
 <?php
 
 $files = PhpCsFixer\Finder::create()
-    ->in([
-        __DIR__ . '/libs',
-    ])
-    ->exclude([
-        __DIR__ . '/libs/compiler/src/Grammar',
-    ])
-;
+    ->in([__DIR__ . '/libs'])
+    ->exclude([__DIR__ . '/libs/compiler/src/Grammar']);
 
 return (new PhpCsFixer\Config())
     ->setRules([
         '@PER-CS2.0' => true,
         '@PER-CS2.0:risky' => true,
-        'strict_param' => true,
         'array_syntax' => [
             'syntax' => 'short',
         ],
+        // PHP 7.4 doesn't support this rule.
+        'trailing_comma_in_multiline' => false,
     ])
     ->setCacheFile(__DIR__ . '/vendor/.php-cs-fixer.cache')
-    ->setFinder($files)
-;
+    ->setFinder($files);
