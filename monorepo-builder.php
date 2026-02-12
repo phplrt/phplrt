f34f62a285d7945b4988394275d258f2cc1315f5
Kirill Nesmeyanov <nesk@xakep.ru>
2026-02-12 19:29:02 +0300

    Add twig package

--- monorepo-builder.php
+++ monorepo-builder.php
@@ -10,25 +10,25 @@ use Symplify\MonorepoBuilder\Release\ReleaseWorker\SetCurrentMutualDependenciesR
 use Symplify\MonorepoBuilder\Release\ReleaseWorker\SetNextMutualDependenciesReleaseWorker;
 use Symplify\MonorepoBuilder\Release\ReleaseWorker\TagVersionReleaseWorker;
 use Symplify\MonorepoBuilder\Release\ReleaseWorker\UpdateBranchAliasReleaseWorker;
+use Symplify\MonorepoBuilder\Release\ReleaseWorker\UpdateReplaceReleaseWorker;
 
 return static function (MBConfig $config): void {
     $config->packageAliasFormat('<major>.<minor>.x-dev');
 
     $config->packageDirectories([
-        __DIR__ . '/libs/compiler',
         __DIR__ . '/libs/components',
         __DIR__ . '/libs/contracts',
-        __DIR__ . '/libs/meta'
+        __DIR__ . '/libs/meta',
     ]);
 
-    $services = $config->services();
-
-    # Release Workers
-    $services->set(SetCurrentMutualDependenciesReleaseWorker::class);
-    $services->set(AddTagToChangelogReleaseWorker::class);
-    $services->set(TagVersionReleaseWorker::class);
-    $services->set(PushTagReleaseWorker::class);
-    $services->set(SetNextMutualDependenciesReleaseWorker::class);
-    $services->set(UpdateBranchAliasReleaseWorker::class);
-    $services->set(PushNextDevReleaseWorker::class);
+    $config->workers([
+        UpdateReplaceReleaseWorker::class,
+        SetCurrentMutualDependenciesReleaseWorker::class,
+        AddTagToChangelogReleaseWorker::class,
+        TagVersionReleaseWorker::class,
+        PushTagReleaseWorker::class,
+        SetNextMutualDependenciesReleaseWorker::class,
+        UpdateBranchAliasReleaseWorker::class,
+        PushNextDevReleaseWorker::class,
+    ]);
 };
