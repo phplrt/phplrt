9bf60a21fbdb7fc3b7d8c8d6360055e056a7826d
Kirill Nesmeyanov <nesk@xakep.ru>
2026-02-19 19:22:13 +0300

    Apply phpcs

--- libs/components/lexer-compiler/src/Definition/Definition.php
+++ libs/components/lexer-compiler/src/Definition/Definition.php
@@ -12,6 +12,7 @@ abstract class Definition implements \Stringable
 
     /**
      * @param int<0, max> $offset
+     *
      * @return $this
      */
     public function setSource(ReadableInterface $source, int $offset): self
