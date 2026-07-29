f1d32d56dfa7af42e0b4db59dcddbfa49746622f
SerafimArts <nesk@xakep.ru>
2026-07-30 01:18:05 +0300

    Fix OutOfRangeException argument naming

--- libs/components/parser/src/Exception/OutOfRangeException.php
+++ libs/components/parser/src/Exception/OutOfRangeException.php
@@ -6,10 +6,10 @@ namespace Phplrt\Parser\Exception;
 
 final class OutOfRangeException extends BufferException
 {
-    public static function becausePositionOutOfRange(int $expected, int $size, ?\Throwable $previous = null): self
+    public static function becausePositionOutOfRange(int $expected, int $lastIndex, ?\Throwable $previous = null): self
     {
         $message = 'Cannot rollback to offset %d, which is outside the buffer range [0..%d]';
-        $message = \sprintf($message, $expected, $size);
+        $message = \sprintf($message, $expected, $lastIndex);
 
         return new self($message, 0, $previous);
     }
