7451976d5e2a77e5ca8e2a4e97c3152e4449170f
SerafimArts <nesk@xakep.ru>
2026-08-26 12:47:52 +0300

    Fix typo in contracts

--- libs/contracts/lexer/src/UserDefinedChannel.php
+++ libs/contracts/lexer/src/UserDefinedChannel.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 namespace Phplrt\Contracts\Lexer;
 
 /**
- * A channel that is defined outside of the basic set of the token channels.
+ * A channel that is defined outside the basic set of the token channels.
  */
 readonly class UserDefinedChannel implements ChannelInterface
 {
